7d3015da5a5f8e4163ba17dc2c0cdf915eda7999
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-11 16:36:34 -0300

    Updated

--- raw_cf.py
+++ raw_cf.py
@@ -1,11 +1,11 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True, locale_code="en") as sb:
-    url = "https://gitlab.com/users/sign_in"
-    sb.activate_cdp_mode(url)
+with SB(uc=True, test=True) as sb:
+    url = "https://seleniumbase.io/apps/turnstile"
+    sb.uc_open_with_reconnect(url)
+    sb.save_screenshot("screenshots/captcha_to_solve.png")
     sb.uc_gui_click_captcha()
-    sb.assert_text("Username", '[for="user_login"]', timeout=3)
-    sb.assert_element('label[for="user_login"]')
-    sb.highlight('button:contains("Sign in")')
-    sb.highlight('h1:contains("GitLab.com")')
-    sb.post_message("SeleniumBase wasn't detected", duration=4)
+    sb.assert_element("img#captcha-success", timeout=3)
+    sb.set_messenger_theme(location="top_left")
+    sb.post_message("SeleniumBase wasn't detected", duration=3)
+    sb.save_screenshot("screenshots/captcha_solved.png")
