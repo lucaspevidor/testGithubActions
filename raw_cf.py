16a6cfe6b30784760220785b569b92e17b9ce367
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-11 17:02:26 -0300

    Update

--- raw_cf.py
+++ raw_cf.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, xvfb=True) as sb:
     url = "https://consulta.trf4.jus.br/trf4/controlador.php?acao=consulta_processual_valida_pesquisa&txtOrigemPesquisa=1&seq=&selForma=NU&txtValor=5039294-34.2024.4.04.7000&txtChave=&selOrigem=PR&txtDataFase=01%2F01%2F1970"
     # sb.uc_open_with_reconnect(url, reconnect_time=20)
     sb.activate_cdp_mode(url)
