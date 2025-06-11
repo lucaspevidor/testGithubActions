560c1a646c86054ea47d47d63b233c0ff1f745ac
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-11 16:42:38 -0300

    updated

--- raw_cf.py
+++ raw_cf.py
@@ -3,9 +3,12 @@ from seleniumbase import SB
 with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/turnstile"
     sb.uc_open_with_reconnect(url)
-    sb.save_screenshot("screenshots/captcha_to_solve.png")
+    sb.save_screenshot("captcha_to_solve.png", "screenshots")
+    print("Clicking on the captcha...")
     sb.uc_gui_click_captcha()
+    sb.save_screenshot("captcha_clicked.png", "screenshots")
+    print("Captcha clicked, waiting for it to be solved...")
     sb.assert_element("img#captcha-success", timeout=3)
     sb.set_messenger_theme(location="top_left")
     sb.post_message("SeleniumBase wasn't detected", duration=3)
-    sb.save_screenshot("screenshots/captcha_solved.png")
+    sb.save_screenshot("captcha_solved.png", "screenshots")
