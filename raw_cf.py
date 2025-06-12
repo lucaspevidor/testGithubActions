43baf861b95c50d7e33e60742635038d22035707
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-12 14:28:24 -0300

    Updated

--- raw_cf.py
+++ raw_cf.py
@@ -31,7 +31,7 @@ canvasInject = """(function injectCanvasOnWebsite() {
     canvas.addEventListener("click", drawCircle);
 })()"""
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, xvfb=True, headed=True, headless=False) as sb:
     url = "https://consulta.trf4.jus.br/trf4/controlador.php?acao=consulta_processual_valida_pesquisa&txtOrigemPesquisa=1&seq=&selForma=NU&txtValor=5039294-34.2024.4.04.7000&txtChave=&selOrigem=PR&txtDataFase=01%2F01%2F1970"
     # sb.uc_open_with_reconnect(url, reconnect_time=20)
     sb.activate_cdp_mode(url)
