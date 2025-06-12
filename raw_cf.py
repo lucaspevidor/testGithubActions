06fb0183e43752276d8cfd5951694af969242bd0
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-12 14:21:58 -0300

    Revert "Updated"
    This reverts commit 3c29bacd4c044eb44d7bbc60b9c1a1b67c092a06.

--- raw_cf.py
+++ raw_cf.py
@@ -7,17 +7,15 @@ with SB(uc=True, test=True) as sb:
     sb.sleep(8)
     sb.save_screenshot("captcha_to_solve.png", "screenshots")
     captchaRect = sb.cdp.get_gui_element_rect("div.cf-turnstile")
-    for varY in range(-50, 100, 15):
-        for varX in range(-40, 100, 15):
-            x = captchaRect["x"] + (captchaRect["height"] / 2) + varX
-            y = captchaRect["y"] + (captchaRect["height"] / 2) + varY
-            print(f"Captcha rect: {captchaRect}")
-            print(f"Clicking at coordinates: ({x}, {y})")
-            sb.cdp.gui_click_x_y(x, y)
-            print("Clicking on the captcha...")
-            sb.save_screenshot(f"captcha_clicked_{varY}_{varX}.png", "screenshots")
-            print("Captcha clicked, waiting for it to be solved...")
+    x = captchaRect["x"] + (captchaRect["height"] / 2) + 15
+    y = captchaRect["y"] + (captchaRect["height"] / 2) - 20
+    print(f"Captcha rect: {captchaRect}")
+    print(f"Clicking at coordinates: ({x}, {y})")
+    sb.cdp.gui_click_x_y(x, y)
+    print("Clicking on the captcha...")
     # sb.uc_gui_click_captcha()
+    sb.save_screenshot("captcha_clicked.png", "screenshots")
+    print("Captcha clicked, waiting for it to be solved...")
     # sb.assert_element("img#captcha-success", timeout=3)
     # sb.set_messenger_theme(location="top_left")
     # sb.post_message("SeleniumBase wasn't detected", duration=3)
