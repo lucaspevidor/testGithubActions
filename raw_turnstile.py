6e32f7e79e8076c9bd2ee17d5746125eb33219e7
lucas-pevidor <lucas.pevidor@gmail.com>
2025-06-12 15:12:51 -0300

    Updated

--- raw_turnstile.py
+++ raw_turnstile.py
@@ -34,13 +34,12 @@ canvasInject = """(function injectCanvasOnWebsite() {
 with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/turnstile"
     url = "https://consulta.trf4.jus.br/trf4/controlador.php?acao=consulta_processual_valida_pesquisa&txtOrigemPesquisa=1&seq=&selForma=NU&txtValor=5039294-34.2024.4.04.7000&txtChave=&selOrigem=PR&txtDataFase=01%2F01%2F1970"
-    sb.uc_open_with_reconnect(url)
+    sb.uc_open_with_reconnect(url, reconnect_time=7)
     sb.save_screenshot("captcha_to_solve.png", "screenshots")
-    sb.sleep(2)
     # sb.execute_script(canvasInject)
     sb.sleep(2)
     sb.save_screenshot("canvas_injected.png", "screenshots")
-    sb.uc_gui_click_x_y(200, 400)
+    # sb.uc_gui_click_x_y(200, 400)
     sb.uc_gui_click_captcha()
     sb.save_screenshot("captcha_clicked.png", "screenshots")
     sb.sleep(3)
